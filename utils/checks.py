4158f66aee1d05fb3877813ca28e1bbeb3496c54
vicky <jvitoclucero@gmail.com>
2026-04-18 19:11:19 +0300

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -9,6 +9,8 @@ def is_owner():
 
 def is_gm():
     def predicate(interaction: discord.Interaction) -> bool:
+        if interaction.user.id == OWNER_ID:
+            return True
         cfg = interaction.client.config
         gm_role_id = cfg.get_gm_role(interaction.guild_id)
         if not gm_role_id:
